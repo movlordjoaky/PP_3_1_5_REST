63fed5ddb6170367d1a3a091bbf6269c78cc60d5
Gurtas <34601321+movlordjoaky@users.noreply.github.com>
2023-08-14 20:16:27 +0300

    warnings

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/CommonUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/CommonUserController.java
@@ -1,15 +1,12 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.services.RoleService;
-import ru.kata.spring.boot_security.demo.services.UserService;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
