07524bed239df413da2f85d8558e994eea08d759
Gurtas <34601321+movlordjoaky@users.noreply.github.com>
2023-08-14 21:13:13 +0300

    Шифрование пароля при создании пользователя

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImpl.java
@@ -2,8 +2,10 @@ package ru.kata.spring.boot_security.demo.services;
 
 import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.dao.UserDAO;
@@ -15,10 +17,12 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class UserServiceImpl implements UserService {
     private final UserDAO userDAOImpl;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserDAO userDAOImpl) {
+    public UserServiceImpl(UserDAO userDAOImpl, @Lazy PasswordEncoder passwordEncoder) {
         this.userDAOImpl = userDAOImpl;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -30,6 +34,11 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public User addUser(User user) {
+        System.out.println("111" + user);
+        String newPassword = passwordEncoder.encode(user.getPassword());
+        System.out.println(newPassword);
+        user.setPassword(newPassword);
+        System.out.println(user);
         return userDAOImpl.addUser(user);
     }
 
