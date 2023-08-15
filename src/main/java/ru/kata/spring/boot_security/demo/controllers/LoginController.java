b84eb55d885a98e9ec67600aab5b06e8c58a7248
Gurtas <34601321+movlordjoaky@users.noreply.github.com>
2023-08-15 15:10:41 +0300

    import bind.annotation.* заменены на частные

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/LoginController.java
@@ -8,7 +8,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import ru.kata.spring.boot_security.demo.models.HtmlResponse;
--- src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserService.java
@@ -1,7 +1,6 @@
 package ru.kata.spring.boot_security.demo.services;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.transaction.annotation.Transactional;
 import ru.kata.spring.boot_security.demo.models.User;
 
 import java.util.List;
